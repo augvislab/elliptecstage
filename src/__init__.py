7fe6e6fae37b8c5568ad6663cdabb0ef0dcea8c8
YutaItoh <yuta.itoh@c.titech.ac.jp>
2021-03-01 13:49:31 +0900

    updated src/__init__.py

--- src/__init__.py
+++ src/__init__.py
@@ -7,7 +7,7 @@ Stage with Resonant Piezoelectric Motors
 """
 import serial
 from enum import Enum
-import src.module
+import src.elliptecstage
 
 version_info = 0, 1, 0
 __version__ = '.'.join(map(str, version_info))
